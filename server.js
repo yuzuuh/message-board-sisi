e7c26ba288fdbc86f807c005fcd5692c18eb778d
Yuzu <tatian.nico.cachito@gmail.com>
2025-11-17 17:10:18 -0300

    Fix: remove mongoose server and use version A

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 'use strict';
-
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
+require('dotenv').config();
 
 const apiRoutes = require('./routes/api');
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,24 +27,8 @@ app.get('/', (req, res) => {
   res.send('Message Board API is running');
 });
 
-// --- MONGODB ---
-const DB = process.env.DB || 'mongodb://127.0.0.1:27017/messageboard';
-
-mongoose.set('strictQuery', false);
-
-mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('DB connected');
-
-    // Evitamos que el servidor arranque en modo test
-    if (process.env.NODE_ENV !== 'test') {
-      const PORT = process.env.PORT || 3000;
-      app.listen(PORT, () =>
-        console.log(`Server running on port ${PORT}`)
-      );
-    }
-  })
-  .catch((err) => console.error('MongoDB error:', err));
+// Render usa process.env.PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
